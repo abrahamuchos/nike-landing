54b7a70338636f5eb7e73b73efb836f537f3bf99
abrahamuchos <j.abraham29@gmail.com>
2024-08-20 17:21:25 -0400

    Add special offer info and image

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,15 +2,20 @@
  *
  * @param {string} label
  * @param {Object|string|null} srcIcon
+ * @param {string} [variant=primary] - Variant can be "primary" or "secondary"
  * @return {JSX.Element}
  * @constructor
  */
-export default function Button({label = 'Click Me', srcIcon}) {
+export default function Button({label = 'Click Me', srcIcon, variant = 'primary'}) {
 
+  const buttonStyles = {
+    primary: 'bg-coral-red border-coral-red text-white',
+    secondary: 'bg-white border-slate-gray text-slate-gray'
+  }
 
   return (
-    <button className='bg-coral-red flex justify-center items-center px-7 py-4 gap-2 font-montserrat text-white text-lg
-      rounded-full leading-none border-coral-red'
+    <button className={`flex justify-center items-center px-7 py-4 gap-2 font-montserrat text-lg border
+      rounded-full leading-none ${buttonStyles[variant] ?? buttonStyles['primary']}`}
     >
       {label}
       {srcIcon && <img src={srcIcon} alt="icon-button" className='ml-2 rounded-full w-5 h-5 bg-white'/>}
