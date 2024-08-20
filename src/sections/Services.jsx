5f597fce1a36bfe6734c2019f0a992c301bba909
abrahamuchos <j.abraham29@gmail.com>
2024-08-20 15:21:17 -0400

    Add cards to services

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,6 +1,16 @@
+import { services } from '../constants'
+import ServiceCard from "../components/ServiceCard.jsx";
 
 export default function Services() {
+
+
   return (
-    <div>Services</div>
+    <section className="max-container flex flex-wrap justify-center gap-9">
+      {services.map(service => (
+        <ServiceCard key={service.label} {...service}/>
+      ))}
+
+
+    </section>
   );
 }
